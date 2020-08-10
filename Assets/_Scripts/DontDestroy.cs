23c4293df9cd6b9171d3fb5e50b35801b2e3cdb9
brandonmifsud-cyber <68404967+brandonmifsud-cyber@users.noreply.github.com>
2020-08-10 12:22:34 +1000

    Added scenes and tidy up game play

---
+++ Assets/_Scripts/DontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    
+    private void Awake()
+    {
+        GameObject[] managers = GameObject.FindGameObjectsWithTag("Manager");
+        if (managers.Length > 1)
+        {
+            Destroy(gameObject); 
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
